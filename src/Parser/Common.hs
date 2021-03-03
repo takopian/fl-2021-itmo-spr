76c833a1c343f7c5632e4d9307b9b88fa77b0392
TigranAkopian <takopian98@gmail.com>
2021-03-03 18:21:53 +0300

    1-4

--- src/Parser/Common.hs
+++ src/Parser/Common.hs
@@ -1,8 +1,8 @@
 module Parser.Common where
 
 import Data.Char (isDigit, isAlpha)
-import Parser.Combinators ( Parser, satisfy )
-import Control.Applicative (some)
+import Parser.Combinators ( Parser, satisfy)
+import Control.Applicative (some, many, (<|>))
 
 digit :: Parser String Char
 digit = satisfy isDigit
@@ -25,8 +25,36 @@ data Associativity = LeftAssoc  -- x `op` y `op` z == (x `op` y) `op` z
                                 -- x `op` y -- ok
                                 -- x `op` y `op` z -- not ok
 
+
+sepByOper :: Parser input op -> Parser input elem -> Parser input ([op], [elem])
+sepByOper sep elem = do
+  h <- elem
+  l_of_pairs <- many ( (,) <$> sep <*> elem)
+  return (map fst l_of_pairs, h : map snd l_of_pairs)
+
+
 uberExpr :: [(Parser i op, Associativity)]
          -> Parser i ast
          -> (op -> ast -> ast -> ast)
          -> Parser i ast
-uberExpr = undefined
+
+uberExpr ((op, NoAssoc):ops) parser build = 
+  ( do
+    left <- uberExpr ops parser build 
+    o <- op
+    right <- uberExpr ops parser build 
+    return $ build o left right
+  ) <|>
+  uberExpr ops parser build 
+
+uberExpr ((op, LeftAssoc):ops) parser build = do
+  (ops, elems) <- sepByOper op (uberExpr ops parser build)
+  let rest = zipWith flip (map build ops) (tail elems)
+  return $ foldl (\ f r -> r f) (head elems) rest 
+
+uberExpr ((op, RightAssoc):ops) parser build = do
+  (ops, elems) <- sepByOper op (uberExpr ops parser build)
+  let rest = zipWith ($) (map build ops) (init elems)
+  return $ foldr ($)  (last elems) rest
+
+uberExpr [] p _ = p
--- src/Parser/Infix.hs
+++ src/Parser/Infix.hs
@@ -1,12 +1,58 @@
 {-# LANGUAGE LambdaCase #-}
 module Parser.Infix where
 
-import Expr ( Expr )
-import Parser.Combinators ( Parser, eof, whitespaces, parseEither )
+import Expr ( Expr (..), Operator, toOp )
+import Parser.Common ( number, alpha, digit, uberExpr, Associativity (..) )
+import Parser.Combinators ( Parser, eof, whitespaces, parseEither, brackets, word, symbol )
+import Control.Applicative (some, many, (<|>))
+
+
+multP = toOp <$> word "*"
+sumP = toOp <$> word "+"
+minusP = toOp <$> word "-"
+divP = toOp <$> word "/"
+powP = toOp <$> word "^"
+eqP = toOp <$> word "=="
+neqP = toOp <$> word "/="
+geP = toOp <$> word ">="
+gtP = toOp <$> word ">"
+leP = toOp <$> word "<="
+ltP = toOp <$> word "<"
+
+noAssocOps :: (Parser String Operator, Associativity)
+noAssocOps = (eqP <|> neqP <|> geP <|> gtP <|> leP <|> ltP, NoAssoc)
+
+sumSubOps :: (Parser String Operator, Associativity)
+sumSubOps = (sumP <|> minusP, LeftAssoc)
+
+multDivOps :: (Parser String Operator, Associativity)
+multDivOps = (multP <|> divP, LeftAssoc)
+
+powOp :: (Parser String Operator, Associativity)
+powOp = (powP, RightAssoc)
+
+ident = do
+  first <- alpha <|> symbol '_'
+  rest <- many (alpha <|> digit <|> symbol '_')
+  return (first : rest)
+
 
 parse :: String -> Either String Expr
 parse =
   parseEither (whitespaces *> parseExpr <* whitespaces <* eof)
 
 parseExpr :: Parser String Expr
-parseExpr = undefined
+parseExpr = 
+  uberExpr [ noAssocOps
+           , sumSubOps
+           , multDivOps
+           , powOp
+           ]
+           ( whitespaces *> (
+             Num <$> number
+             <|> Ident <$> ident
+             <|> symbol '(' *> parseExpr <* symbol ')'
+             ) <* whitespaces
+           )
+           BinOp
+
